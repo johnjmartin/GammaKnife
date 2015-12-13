1dbab119fb50460c2fff277184eda942c35226ac
unknown <8jjc3@g248-061.caslab.queensu.ca>
2015-12-13 16:45:25 -0500

    updated

--- computePointDose.m
+++ computePointDose.m
@@ -1,23 +1,29 @@
-function [ pointDoseValue ] = computePointDose( head, helmet, OAR, isocenterPoint, pointOfInterest )
+function [ pointDoseValue ] = computePointDose( head, helmet, OAR, isocentrePoint, pointOfInterest )
 %COMPUTEPOINTPOSE Summary of this function goes here
 %   Detailed explanation goes here
+    global safetyTable;
+    
     pointDoseValue = 0;
     totalLon = floor(360 / helmet(1));
     
     %one beam at the top of the helmet
     if mod(90,helmet(1)) == 0 
-        totalLat = (90 / helmet(1) - 1);
+        totalLat = (90 / helmet(1));
         pointDoseValue = pointDoseValue + ...
-                computePointDosefromBeam( head, helmet(2), 0, 90, isocenterPoint, pointOfInterest );
+                computePointDosefromBeam( head, helmet(2), 0, 90, isocentrePoint, pointOfInterest );
     %no beams at the top of the helmet
     else
-        totalLat =  floor(90 / helmet(1));
+        totalLat =  floor(90 / helmet(1)) + 1;
     end    
+       
     
     for lon = 1:totalLon
         for lat = 1:totalLat
-            pointDoseValue = pointDoseValue + ...
-                computePointDosefromBeam( head, helmet(2), lon*helmet(1), lat*helmet(1), isocenterPoint, pointOfInterest );
+            %beam is safe, can be used
+            if(safetyTable(lat+1,lon+1)) == 1
+                pointDoseValue = pointDoseValue + ...
+                computePointDosefromBeam( head, helmet(2), (lon+1)*helmet(1), (lat+1)*helmet(1), isocentrePoint, pointOfInterest );
+            end              
         end
     end
     
--- computePointDosefromBeam.m
+++ computePointDosefromBeam.m
@@ -1,16 +1,29 @@
 function [ pointDoseValue ] = computePointDosefromBeam( head, beamRadius, beamLongitude, beamLatitude, isocentrePoint, pointOfInterest )
 %COMPUTEPOINTDOSEFROMBEAM Summary of this function goes here
 %   Detailed explanation goes here
+    
+    global depthDoseTable;
+    global radialDoseTable;
+    global d0;
+    
     beamDirection = computeBeamDirectionVector(beamLongitude, beamLatitude);
     skinEntryPoint = computeSkinEntryPoint( head, beamLongitude, beamLatitude, isocentrePoint);
     radialDistance = computeRadialDistance(pointOfInterest, [skinEntryPoint, beamDirection]);
-    radialDose = radialDoseTable(radialDistance/beamRadius);
-    %http://math.oregonstate.edu/home/programs/undergrad/CalculusQuestStudyGuides/vcalc/dotprod/dotprod.html
-    t = dot(pointOfInterest - skinEntryPoint,isocentrePoint - skinEntryPoint)/norm(isocentrePoint - skinEntryPoint);
-    pointOnBeam = skinEntryPoint + t.*beamDirection;
-    depth = norm(skinEntryPoint - pointOnBeam);
-    depthDose = depthDoseTable(depth/d0);
-    pointDoseValue = radialDose * depthDose;
+    
+    %radial distance is close enough to have a dose that isn't 0
+    if (radialDistance/beamRadius) < 1.5
+        radialIndex = findTableIndex(radialDistance/beamRadius,radialDoseTable(:,1));
+        radialDose = radialDoseTable(radialIndex,2);
+        %http://math.oregonstate.edu/home/programs/undergrad/CalculusQuestStudyGuides/vcalc/dotprod/dotprod.html
+        t = dot(pointOfInterest - skinEntryPoint,isocentrePoint - skinEntryPoint)/norm(isocentrePoint - skinEntryPoint);
+        pointOnBeam = skinEntryPoint + t.*beamDirection;
+        depth = norm(skinEntryPoint - pointOnBeam);
+        depthIndex = findTableIndex(depth/d0,depthDoseTable(:,1)); 
+        depthDose = depthDoseTable(depthIndex);
+        pointDoseValue = radialDose * depthDose;
+    else
+        pointDoseValue = 0;
+    end
 
 end
 
